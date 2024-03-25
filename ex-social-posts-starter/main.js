a713fe85a443fa102251d75485eff043afcd66e7
Casimiro Moliterni <moliternicasimiro@hotmail.it>
2024-03-25 15:56:46 +0100

    add : milistone 1

--- ex-social-posts-starter/main.js
+++ ex-social-posts-starter/main.js
@@ -1,3 +1,11 @@
+
+// Milestone 1 - Prendendo come riferimento il layout di esempio presente nell'html, stampiamo i post del nostro feed.
+// Milestone 2 - Se clicchiamo sul tasto "Mi Piace" cambiamo il colore al testo del bottone e incrementiamo il counter dei likes relativo.
+// Salviamo in un secondo array gli id dei post ai quali abbiamo messo il like.
+// BONUS
+// 1. Formattare le date in formato italiano (gg/mm/aaaa)
+// 2. Gestire l'assenza dell'immagine profilo con un elemento di fallback che contiene le iniziali dell'utente (es. Luca Formicola > LF).
+// 3. Al click su un pulsante "Mi Piace" di un post, se abbiamo già cliccato dobbiamo decrementare il contatore e cambiare il colore del bottone.
 const posts = [
     {
         "id": 1,
@@ -55,3 +63,64 @@ const posts = [
         "created": "2021-03-05"
     }
 ];
+
+// Milestone 1 - Prendendo come riferimento il layout di esempio presente nell'html, stampiamo i post del nostro feed.
+
+const myContainer = document.querySelector('#container');
+console.log(myContainer);
+posts.forEach((singlePost) => {
+    const templatePost = generatePost(singlePost);
+    myContainer.innerHTML += templatePost;
+    });
+    
+    
+    
+    
+    // funzione che genera postCard univoci 
+    function generatePost(thisPost){
+        const {id,content,media,author,likes,created} = thisPost ;
+       const teamPost = `
+       <div class="post">
+                <div class="post__header">
+                    <div class="post-meta">                    
+                        <div class="post-meta__icon">
+                            ${generateImg(author.image)}                   
+                        </div>
+                        <div class="post-meta__data">
+                            <div class="post-meta__author">${author.name}</div>
+                            <div class="post-meta__time">${created}</div>
+                        </div>                    
+                    </div>
+                </div>
+                <div class="post__text">Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.</div>
+                <div class="post__image">
+                    <img src="https://unsplash.it/600/300?image=171" alt="">
+                </div>
+                <div class="post__footer">
+                    <div class="likes js-likes">
+                        <div class="likes__cta">
+                            <a class="like-button  js-like-button" href="#" data-postid="1">
+                                <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
+                                <span class="like-button__label">Mi Piace</span>
+                            </a>
+                        </div>
+                        <div class="likes__counter">
+                            Piace a <b id="like-counter-1" class="js-likes-counter">${likes}</b> persone
+                        </div>
+                    </div> 
+                </div>            
+            </div>
+       `;
+       return teamPost;
+    };
+
+    // funzione img assente 
+     function generateImg (image){
+        let imgString;
+        if(image){
+            imgString = `<img class="profile-pic" src="${image}" alt="Phil Mangione"> `;
+        }else{
+            imgString = `<span class = "profile-pic-default">LF</span>`;
+        }
+        return imgString;
+     }
